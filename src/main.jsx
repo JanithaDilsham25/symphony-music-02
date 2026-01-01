eff71021ab25bae96161ecb11afee1e10dd02bce
ParindyaHewage <parindyahewage00@gmail.com>
2026-01-01 23:14:29 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <GridOverlay />
+      {/* <GridOverlay /> */}
       <App />
-      
-    </BrowserRouter>
   </StrictMode>,
 )
