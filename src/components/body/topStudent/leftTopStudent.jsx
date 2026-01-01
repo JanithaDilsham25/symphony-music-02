9dc46a881546316304d6016f8de949c085ac4bcc
ParindyaHewage <parindyahewage00@gmail.com>
2026-01-01 22:48:48 +0530

    change top student

--- src/components/body/topStudent/leftTopStudent.jsx
+++ src/components/body/topStudent/leftTopStudent.jsx
@@ -1,57 +1,4 @@
-// import man from "../../../assets/topStudent/man.png";
 
-// export default function LeftTopStudent({ mobile }) {
- 
-//   if (mobile) {
-//     return (
-//       <div className="w-[398px] h-[278px] relative ">
-//         <div className="w-[367.46px] h-[141.33px] bg-[#FFE999]
-//             rounded-[12.48px] border border-[#E08A00]
-//             absolute bottom-0 right-0 pr-[8px] flex justify-end items-center">
-          
-//           <div className="w-[166px]  text-start text-[#454545]">
-//             <p className="font-semibold text-[20px]">Emily Johnson</p>
-//             <div className="text-[12px] font-semibold mt-[6px] space-y-[4px]">
-//               <p>Instrument : Piano</p>
-//               <p>Class / Level : Grade 12</p>
-//               <p>Results : AAA+</p>
-//             </div>
-//           </div>
-//         </div>
-
-//         <img
-//           src={man}
-//           className="w-[224.16px] h-[277.68px] absolute bottom-0 left-0 object-cover"
-//         />
-//       </div>
-//     );
-//   }
-
-//   //desktop
-//   return (
-//     <div className="w-[788.5px] h-[551px] relative">
-//       <div className="w-[728px] h-[280px] bg-[#FFE999]
-//           rounded-[24.73px] border border-[#E08A00]
-//           absolute bottom-0 right-0 px-[64px] py-[43px] flex justify-end">
-        
-//         <div className="w-[266px] text-start text-[#454545]">
-//           <p className="font-semibold text-[32px]">Emily Johnson</p>
-//           <div className="text-[20px] font-semibold mt-[16px] space-y-[6px]">
-//             <p>Student ID : ST002/2020</p>
-//             <p>Instrument : Piano</p>
-//             <p>Class / Level : Grade 12</p>
-//             <p>Results : AAA+</p>
-//           </div>
-//         </div>
-//       </div>
-
-//       <img
-//         src={man}
-//         className="w-[444.11px] h-[550.14px] absolute bottom-0 left-0 object-cover"
-//       />
-//     </div>
-//   );
-// }
 
 
 import man from "../../../assets/topStudent/man.png";
@@ -59,21 +6,24 @@ import man from "../../../assets/topStudent/man.png";
 export default function LeftTopStudent() {
   return(
     <>
-      <div className="lg:w-[41vw] w-[92vw] sm:w-[50vw] aspect-[1.43] relative bg-amber-10">
-        <div className="lg:w-[38vw] w-[85vw] sm:w-[46vw] aspect-[2.6] bg-[#FFE999CC] absolute bottom-0 right-0 rounded-[24px] flex flex-col justify-center items-end border-primary border-2">
-             
-              <div className="md:pr-[3.33vw] pr-[5.11vw] text-[#454545] xl:gap-[24px]">
-                  <p className="font-semibold xl:text-[26px] lg:text-[20px] text-[18px] ">Emily Johnson</p>
-                  <div className="lg:w-[13.9vw] xl:text-[16px] lg:text-[12px] text-[12px] flex flex-col xl:gap-2">
+      <div className="md:w-[41vw] w-[92vw] aspect-[1.43] relative bg-amber-10 sm:scale-90 md:scale-110 lg:scale-100">
+        <div className="md:w-[38vw] w-[85vw]  aspect-[2.6] bg-[#FFE999CC] absolute bottom-0 right-0 rounded-[24px] flex flex-col justify-center items-end border-primary border-2">
+
+            <div className="md:w-[20vw] w-[41vw]  flex justify-center items-center">
+              <div className="flex flex-col text-[#454545] xl:gap-[1rem]">
+                  <p className="font-semibold xl:text-[26px] lg:text-[20px] md:text-[16px] sm:text-[26px] text-[18px] ">Emily Johnson</p>
+                  <div className=" xl:text-[16px] lg:text-[12px] md:text-[10px] sm:text-[16px] text-[12px] flex flex-col xl:gap-1">
                     <p>Student ID : ST002/2020</p>
                    <p >Instrument : Piano</p>
                   <p>Class / Level : Grade 12</p>
                   <p>Results : AAA+</p>
                   </div>
               </div>
+            </div> 
+            
         </div>
         <div className="">
-              <img  src={man} className="lg:w-[23vw] w-[52vw] sm:w-[28vw]  absolute bottom-0 left-0 object-cover"/>
+              <img  src={man} className="md:w-[23vw] w-[52vw]  absolute bottom-0 left-0 object-cover"/>
         </div>
       </div>
       
--- src/components/body/topStudent/rightTopStudent.jsx
+++ src/components/body/topStudent/rightTopStudent.jsx
@@ -1,57 +1,3 @@
-// import girl from "../../../assets/topStudent/girl.png";
-
-// export default function RightTopStudent({ mobile }) {
- 
-//   if (mobile) {
-//     return (
-//       <div className="w-[398px] h-[278px] relative">
-//         <div className="w-[371.94px] h-[141.06px] bg-[#FFC373]
-//             rounded-[24.73px] border border-[#FF9100]
-//             absolute bottom-0 left-0 px-[22px] py-[21.66px] text-start text-[#454545]">
-          
-//           <p className="font-semibold text-[20px]">Sarah Johnson</p>
-//           <div className="text-[14px] font-semibold mt-[8px] space-y-[4px]">
-//             <p>Instrument : Guitar</p>
-//             <p>Class / Level : Grade 9</p>
-//             <p>Results : AAA</p>
-//           </div>
-//         </div>
-
-//         <div className="overflow-hidden">
-//             <img
-//           src={girl}
-//           className="w-[204.56px] h-[275.85px] absolute bottom-0 right-0 object-cover object-top"
-//         />
-//         </div>
-//       </div>
-//     );
-//   }
-
-
-//   return (
-//     <div className="w-[779px] h-[540px] relative">
-//       <div className="w-[728px] h-[280px] bg-[#FFC373]
-//           rounded-[24.73px] border border-[#FF9100]
-//           absolute bottom-0 left-0 px-[64px] py-[43px] text-start text-[#454545] items-center">
-        
-//         <p className="font-semibold text-[32px]">Sarah Johnson</p>
-//         <div className="text-[20px] font-semibold mt-[16px] space-y-[6px]">
-//           <p>Student ID : ST001/2020</p>
-//           <p>Instrument : Guitar</p>
-//           <p>Class / Level : Grade 9</p>
-//           <p>Results : AAA</p>
-//         </div>
-//       </div>
-
-//       <div  className="overflow-hidden">
-//         <img
-//         src={girl}
-//         className="w-[400.39px] h-[600px] absolute bottom-0 right-0 object-cover object-top"
-//       />
-//       </div>
-//     </div>
-//   );
-// }
 
 
 import girl from "../../../assets/topStudent/girl.png";
@@ -59,21 +5,22 @@ import girl from "../../../assets/topStudent/girl.png";
 export default function RightTopStudent() {
         return(
           <>
-               <div className="lg:w-[41vw] w-[92vw] sm:w-[50vw] aspect-[1.46] relative bg-amber-10">
-                      <div className="lg:w-[38vw] w-[85vw] sm:w-[46vw] aspect-[2.64] bg-[#FFE999CC] absolute bottom-0 left-0 rounded-[24px] flex flex-col justify-center items-start border-primary border-2">
-                           
-                            <div className="lg:pl-[3.33vw] pl-[5.11vw] text-[#454545] xl:gap-[24px]">
-                                <p className="font-semibold xl:text-[26px] lg:text-[18px] text-[18px] ">Emily Johnson</p>
-                                <div className="lg:w-[13.9vw] xl:text-[16px] lg:text-[12px] text-[12px]  flex flex-col xl:gap-2">
+               <div className="md:w-[41vw] w-[92vw] aspect-[1.46] relative bg-amber-10 sm:scale-90 md:scale-110 lg:scale-100">
+                      <div className="md:w-[38vw] w-[85vw]  aspect-[2.64] bg-[#FFE999CC] absolute bottom-0 left-0 rounded-[24px] flex flex-col justify-center items-start border-primary border-2">
+                         <div className=" md:w-[20vw] w-[41vw] flex justify-center items-center">
+                            <div className="flex flex-col  text-[#454545]  xl:gap-[1rem] ">
+                                <p className="font-semibold xl:text-[26px] lg:text-[20px] md:text-[18px] sm:text-[26px] text-[18px] ">Emily Johnson</p>
+                                <div className="xl:text-[16px] lg:text-[12px] md:text-[10px] sm:text-[16px] text-[12px]  flex flex-col xl:gap-1">
                                   <p>Student ID : ST002/2020</p>
                                  <p >Instrument : Piano</p>
                                 <p>Class / Level : Grade 12</p>
                                 <p>Results : AAA+</p>
                                 </div>
                             </div>
+                          </div>  
                       </div>
                       <div  className="overflow-hidden">
-                        <img src={girl}  className="lg:w-[21vw] w-[48vw] sm:w-[26vw] aspect-[0.74] absolute bottom-0 right-0 object-cover object-top"/>
+                        <img src={girl}  className="md:w-[21vw] w-[48vw]  aspect-[0.74] absolute bottom-0 right-0 object-cover object-top"/>
                       </div>
                 </div> 
           
