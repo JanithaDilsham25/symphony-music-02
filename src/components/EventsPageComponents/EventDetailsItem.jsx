41087b11d20de22ada83034ca793bc6ac5e9ded1
dasunithimasha <dasunithimasha25@gmail.com>
2026-01-10 22:26:39 +0530

    finish event card

--- src/components/EventsPageComponents/EventDetailsItem.jsx
+++ src/components/EventsPageComponents/EventDetailsItem.jsx
@@ -1,9 +1,9 @@
 function EventDetailItem({ img, text }) {
   return (
-    <div className="flex items-center  text-gray-700  w-[378.34564208984375px] h-6 gap-[7.37px]">
+    <div className="flex items-center  text-gray-700 gap-[7.37px]">
    
-      <img src={img} className="w-6 h-6 "></img>
-      <span className="h-6 font-secondary font-normal text-[16px] tracking-normal leading-6 text-secondary">{text}</span>
+      <img src={img} className="w-4 h-4 "></img>
+      <span className=" font-secondary font-normal text-[0.9rem] tracking-normal leading-3 text-secondary">{text}</span>
     </div>
   );
 }
